44cf2aea629b8322289196b3002e5f30805c3737
jrivera <jrivera@picernefl.com>
2015-09-12 20:36:43 -0400

    Add class
    Added class to the main content div

--- page.php
+++ page.php
@@ -12,7 +12,7 @@
 
   <main id="main" class="site-main" role="main"> <!-- role="main" added for ARIA compatibility on Internet Explorer -->
     <div class="row">
-				<div class="large-9 columns" role="content">
+				<div class="large-9 columns content-wrapper" role="content">
     <?php while ( have_posts() ) : the_post(); ?>
 
       <?php get_template_part( 'template-parts/content', 'page' ); ?>
