9030ad77c7bc701e3255a96391712934d5b7d0bf
jrivera <jrivera@picernefl.com>
2015-09-18 06:37:44 -0400

    Setup My Course Page Template

---
+++ page-my-courses.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Template Name: My Courses
+ *
+ * Template to display the My Courses Dashboard
+ *
+ * @package celebrationhealth
+ */
+
+ get_header(); ?>
+
+<div class="page-wrapper">
+
+  <main id="main" class="site-main" role="main"> <!-- role="main" added for ARIA compatibility on Internet Explorer -->
+    <div class="row">
+				<div class="large-9 columns content-wrapper" role="content">
+    <?php while ( have_posts() ) : the_post(); ?>
+
+      <?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+    <?php endwhile; // End of the loop. ?>
+        </div> <!-- end . large-9 columns -->
+
+        <?php get_sidebar(); //display the sidebar ?>
+
+    </div> <!-- end .row -->
+  </main>
+
+</div> <!-- end .page-wrapper -->
+
+
+
+<?php get_footer(); ?>
