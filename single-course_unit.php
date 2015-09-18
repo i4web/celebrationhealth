4ae6f28c733fcac1705c4539800de760382746dc
jrivera <jrivera@picernefl.com>
2015-09-18 06:37:58 -0400

    Setup Course Unit Template

---
+++ single-course_unit.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The Course Unit template file for celebrationhealth.
+ *
+ * @package celebrationhealth
+ */
+
+get_header(); ?>
+
+
+<div class="page-wrapper">
+
+  <main id="main" class="site-main" role="main"> <!-- role="main" added for ARIA compatibility on Internet Explorer -->
+    <div class="row">
+				<div class="large-10 columns small-centered content-wrapper" role="content">
+    <?php while ( have_posts() ) : the_post(); ?>
+
+      <?php get_template_part( 'template-parts/content', 'course-unit' ); ?>
+
+    <?php endwhile; // End of the loop. ?>
+        </div> <!-- end . large-9 columns -->
+    </div> <!-- end .row -->
+  </main>
+
+</div> <!-- end .page-wrapper -->
+
+<?php get_footer(); ?>
---
+++ template-parts/content-course-unit.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package celebrationhealth
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" class="course-unit-wrapper">
+<?php the_content(); ?>
+
+</article>
