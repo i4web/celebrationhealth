a7c926bf5778743f37a3d711fafaf1768095c3f3
jrivera <jrivera@i-4web.com>
2015-09-26 10:17:28 -0400

    add the course_unit metadata to template
    Added the Vimeo iframe and integrated the Video ID metadata into the
    iframe
    
    Added the Video Length metadata to the template

--- template-parts/content-course-unit.php
+++ template-parts/content-course-unit.php
@@ -10,6 +10,10 @@ $unit_parent_data = WPCW_units_getAssociatedParentData($post->ID);
 
 $user_id = get_current_user_id();
 
+$video_id = get_post_meta( $post->ID, 'video-id', true ); //Grab the Video ID for displaying in the Vimeo iFrame
+$video_length = get_post_meta ( $post->ID, 'video-length', true ); //Grab the Video length for displaying in the video details
+
+
 //var_dump($unit_parent_data);
 
 //Check if the Unit is complete
@@ -23,7 +27,7 @@ $unit_status = I4Web_LMS()->i4_wpcw->i4_is_unit_complete( $unit_parent_data->cou
   <div class="row">
     <div class="large-8 columns video-content-wrapper">
       <div class="flex-video">
-        <iframe src="https://player.vimeo.com/video/114881210" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> <p><a href="https://vimeo.com/114881210">Florida Hospital Unveils Rooms of New Women&rsquo;s Hospital</a> from <a href="https://vimeo.com/flhospital">Florida Hospital</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+        <iframe src="https://player.vimeo.com/video/<?php echo $video_id; ?>" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
       </div>
     </div> <!-- end .video-content-wrapper -->
     <div class="large-4 columns video-details-wrapper">
@@ -39,7 +43,7 @@ $unit_status = I4Web_LMS()->i4_wpcw->i4_is_unit_complete( $unit_parent_data->cou
         </tr>
         <tr>
           <td><strong>Length</strong></td>
-          <td>15:30</td>
+          <td><?php echo $video_length; ?></td>
         </tr>
       </table>
 
