88d6f2f56f1593fdacb6ee063e26d41748e52feb
jrivera <jrivera@i-4web.com>
2015-10-09 00:36:09 -0400

    Tweak Nav
    Tweaked the navigation menu

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 
     <?php wp_head(); ?>
   </head>
+
   <body <?php body_class(); ?>>
     <div class="i4-site-wrapper">
 
@@ -29,36 +30,38 @@
        ?>
 
       <!-- Begin Navbar -->
-      <nav class="top-bar" data-topbar role="navigation">
-      <ul class="title-area">
-        <li class="name">
-          <a href="<?php echo get_bloginfo('url');?>" class="nav-logo" title="<?php echo get_bloginfo('title');?>"><img src="<?php echo $nav_logo;?>"/></a>
-        </li>
-         <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-        <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-      </ul>
-
-      <section class="top-bar-section">
-        <!-- Right Nav Section -->
-        <ul class="right">
-          <li class="active"><a href="<?php echo  get_home_url(); ?>" title="View Assigned Courses">My Courses</a></li>
-
-          <?php foreach ($i4_user_courses as $i4_user_course){
-
-            $course_permalink = sanitize_title($i4_user_course->course_title);
-
-            echo '<li><a href="'.$course_permalink.'">'. $i4_user_course->course_title .'</a><li>';
-          }
-
-          ?>
-
-          <li class="has-dropdown">
-            <a href="#">Welcome, Jonathan</a>
-            <ul class="dropdown">
-              <li><a href="/profile">Your Profile</a></li>
-              <li><a href="<?php echo wp_logout_url( get_permalink() ); ?>">Logout</a></li>
-            </ul>
+      <div class="sticky">
+        <nav class="top-bar" data-topbar role="navigation" data-options="sticky_on: [small,medium,large]">
+        <ul class="title-area">
+          <li class="name">
+            <a href="<?php echo get_bloginfo('url');?>" class="nav-logo" title="<?php echo get_bloginfo('title');?>"><img src="<?php echo $nav_logo;?>"/></a>
           </li>
+           <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+          <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
         </ul>
-      </section>
-    </nav>
+
+        <section class="top-bar-section">
+          <!-- Right Nav Section -->
+          <ul class="right">
+            <li class="active"><a href="<?php echo  get_home_url(); ?>" title="View Assigned Courses">My Courses</a></li>
+
+            <?php foreach ($i4_user_courses as $i4_user_course){
+
+              $course_permalink = sanitize_title($i4_user_course->course_title);
+
+              echo '<li><a href="'.$course_permalink.'">'. $i4_user_course->course_title .'</a><li>';
+            }
+
+            ?>
+
+            <li class="has-dropdown">
+              <a href="#">Welcome, Jonathan</a>
+              <ul class="dropdown">
+                <li><a href="/profile">Your Profile</a></li>
+                <li><a href="<?php echo wp_logout_url( get_permalink() ); ?>">Logout</a></li>
+              </ul>
+            </li>
+          </ul>
+        </section>
+      </nav>
+    </div>
