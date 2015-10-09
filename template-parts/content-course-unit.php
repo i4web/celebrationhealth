536fd9c58e8ac4430e8297dcae23b84ec0cbedb2
jrivera <jrivera@i-4web.com>
2015-10-09 00:37:19 -0400

    Tweak the course unit template
    Changed how we grab the user ID and added additional variables from the
    DB for the coordinator.
    
    Also I finalized the Coordinator Area on the template

--- template-parts/content-course-unit.php
+++ template-parts/content-course-unit.php
@@ -8,7 +8,8 @@
 //Store the Parent Data for the Unit for use.
 $unit_parent_data = WPCW_units_getAssociatedParentData($post->ID);
 
-$user_id = get_current_user_id();
+$i4_user = wp_get_current_user();
+$user_id = $i4_user->ID;
 
 $video_id = get_post_meta( $post->ID, 'video-id', true ); //Grab the Video ID for displaying in the Vimeo iFrame
 $video_length = get_post_meta( $post->ID, 'video-length', true ); //Grab the Video length for displaying in the video details
@@ -19,6 +20,11 @@ $video_description = get_post_meta( $post->ID, 'video-description', true ); //Gr
 
 //Check if the Unit is complete
 $unit_status = I4Web_LMS()->i4_wpcw->i4_is_unit_complete( $unit_parent_data->course_id, $user_id, $post->ID );
+
+//Retrieve the Course Coordinator information
+$i4_course_coordinator = I4Web_LMS()->i4_coordinators->i4_lms_get_coordinator( $unit_parent_data->course_id );
+$i4_coordinator_name = $i4_course_coordinator->coordinator_name;
+$i4_coordinator_email = $i4_course_coordinator->coordinator_email;
 
 ?>
 
@@ -37,6 +43,30 @@ $unit_status = I4Web_LMS()->i4_wpcw->i4_is_unit_complete( $unit_parent_data->cou
     </div> <!-- end .video-content-wrapper -->
     <div class="large-4 columns video-details-wrapper">
       <h4 class="video-details-title">Video Details</h4>
+      <div class="row">
+        <div class="course-coordinator-wrapper large-12 columns">
+          <div class="course-coordinator-details">
+              <div class="course-coordinator-img">
+                <img src="<?php echo $i4_course_coordinator->coordinator_img; ?>" alt="Coordinator Name" class="img-circle">
+              </div>
+                <h5><?php echo $i4_coordinator_name; ?> <br/><small>Course Coordinator</small></h5>
+                <a href="#" class="button grey tiny" data-reveal-id="askCoordinatorModal">Ask a Question</a>
+
+                <div id="askCoordinatorModal" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+                    <p class="text-center"><img src="<?php echo $i4_course_coordinator->coordinator_img; ?>" alt="<?php echo $i4_coordinator_name;?>" class="img-circle"></p>
+                    <h5 class="text-center"><?php echo $i4_coordinator_name; ?><br/><small>Course Coordinator</small></h5>
+                  <p id="modalTitle" class="text-center">I'm here to assist you with any questions or concerns that you may have. Just fill out the form below to email me directly.</p>
+                  <hr>
+                  <?php echo do_shortcode( '[gravityform id="1" title="false" description="false" ajax="true" field_values="patient_username='.$i4_user->user_login.'&amp;coordinator_email='.$i4_coordinator_email.'&amp;user_email='.$i4_user->user_email.'"]' ); ?>
+                  <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+                </div>
+
+          </div>
+        </div>
+      </div>
+
+      <hr>
+
       <table class="video-details">
         <tr>
           <td><strong>Course</strong></td>
@@ -54,7 +84,7 @@ $unit_status = I4Web_LMS()->i4_wpcw->i4_is_unit_complete( $unit_parent_data->cou
         <?php } ?>
       </table>
 
-<?php echo I4Web_LMS()->i4_wpcw_front_end_unit->i4_lms_unit_actions(); ?>
+      <?php echo I4Web_LMS()->i4_wpcw_front_end_unit->i4_lms_unit_actions();      ?>
 
     </div>
   </div> <!-- end row -->
