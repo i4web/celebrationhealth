365350d04fe8dcab656f72311f59b74f65d14211
jrivera <jrivera@i-4web.com>
2015-10-11 12:51:03 -0400

    Setup Fullwidth Page Template

---
+++ page-fullwidth.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Template Name: Fullwdith
+ *
+ * Template for Fullwidth pages
+ *
+ * @package celebrationhealth
+ */
+
+ get_header(); ?>
+
+<div class="page-wrapper">
+
+  <main id="main" class="site-main" role="main"> <!-- role="main" added for ARIA compatibility on Internet Explorer -->
+    <div class="row">
+				<div class="large-12 columns content-wrapper" role="content">
+    <?php while ( have_posts() ) : the_post(); ?>
+
+      <?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+    <?php endwhile; // End of the loop. ?>
+  </div> <!-- end . large-12 columns -->
+
+    </div> <!-- end .row -->
+  </main>
+
+</div> <!-- end .page-wrapper -->
+
+
+
+<?php get_footer(); ?>
