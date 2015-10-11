a7e481c767925448eecd4e376243cc302fc23a58
jrivera <jrivera@i-4web.com>
2015-10-11 12:52:42 -0400

    New My Courses content file

---
+++ template-parts/content-my-courses-page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template used for displaying page content in page-my-courses-page.php
+ *
+ * @package celebrationhealth
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+<?php the_content(); ?>
+
+</article>
