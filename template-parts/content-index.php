e938f4ab52d425d5902d97c2ceb30599cb45c381
jrivera <jrivera@i-4web.com>
2015-10-16 09:57:11 -0400

    Tweak the Posts loop template

--- template-parts/content-index.php
+++ template-parts/content-index.php
@@ -11,8 +11,17 @@ $i4_post_title = get_the_title();
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <div class="page-content-wrapper">
-    <div class="page-title"><h3><?php echo $i4_post_title; ?></h3></div>
-    <?php echo i4_lms_posted_on(); ?>
+      <div class="featured-img-wrapper">
+      <?php if ( has_post_thumbnail() ) {
+          $post_featured_img = get_the_post_thumbnail( get_the_ID(), 'full' );
+          echo $post_featured_img;
+      } ?>
+      </div>
+      <div class="post-title">
+          <h3><?php echo $i4_post_title; ?></h3>
+          <?php echo i4_lms_posted_on(); ?>
+      </div>
+
     <?php the_excerpt(); ?>
     <a href="<?php echo get_the_permalink();?>" class="read-more" title="<?php echo $i4_post_title; ?>">Read More</a>
   </div>
