01d7b335ea378976e98222c33ce6754abd2ebfc9
jrivera <jrivera@i-4web.com>
2015-10-17 00:18:33 -0400

    Tweak Category and Index templates

--- category.php
+++ category.php
@@ -14,10 +14,11 @@
 get_header(); ?>
 
 <div class="page-wrapper">
-
   <main id="main" class="site-main" role="main"> <!-- role="main" added for ARIA compatibility on Internet Explorer -->
     <div class="row">
 				<div class="large-9 columns content-wrapper" role="content">
+          <h4 class="category-title">Category: <?php single_cat_title(); ?></h4>
+
     <?php while ( have_posts() ) : the_post(); ?>
 
       <?php get_template_part( 'template-parts/content', 'index' ); ?>
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -9,16 +9,17 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <div class="page-content-wrapper">
-      <div class="featured-img-wrapper">
-      <?php if ( has_post_thumbnail() ) {
-          $post_featured_img = get_the_post_thumbnail( get_the_ID(), 'full' );
-          echo $post_featured_img;
-      } ?>
-      </div>
     <div class="post-title">
         <h3><?php echo get_the_title();?></h3>
         <?php echo i4_lms_posted_on(); ?>
     </div>
+    <div class="featured-img-wrapper">
+    <?php if ( has_post_thumbnail() ) {
+        $post_featured_img = get_the_post_thumbnail( get_the_ID(), 'full' );
+        echo $post_featured_img;
+    } ?>
+    </div>
+
     <?php the_content(); ?>
   </div>
 
