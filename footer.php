29d51524317127dfd04a5a11dc1237f1f0e069c5
jrivera <jrivera@i-4web.com>
2015-10-21 23:30:30 -0400

    Finalize Footer Layout

--- footer.php
+++ footer.php
@@ -10,38 +10,41 @@
  <footer>
    <div class="footer-menu-wrapper">
      <div class="row">
-       <div class="columns large-3">
-         <h5>Resources</h5>
-         <a href="#">Forms</a> <br> <a href="#">Maps</a>
-       </div>
-       <div class="columns large-6">
-         <h5>Creation Health</h5>
-         <div class="creation-health-logo">
-           <div class="creation-health-acronym-wrapper large-8 columns">
-             <span class="creation-acronym creation-green has-tip tip-top" data-tooltip aria-haspopup="true" title="CHOICE - Make Wise Decisions for Healthy Living">C</span>
-             <span class="creation-acronym creation-orange has-tip tip-top" data-tooltip aria-haspopup="true" title="REST - Enjoy Replenishing Sleep and Relaxation">R</span>
-             <span class="creation-acronym creation-green has-tip tip-top" data-tooltip aria-haspopup="true" title="ENVIRONMENT - Create a Nurturing, Rejuvenating Surrounding">E</span>
-             <span class="creation-acronym creation-orange has-tip tip-top" data-tooltip aria-haspopup="true" title="ACTIVITY - Put Your Body in Motion">A</span>
-             <span class="creation-acronym creation-pink has-tip tip-top" data-tooltip aria-haspopup="true" title="TRUST - Express Your Faith in God">T</span>
-             <span class="creation-acronym creation-red has-tip tip-top" data-tooltip aria-haspopup="true" title="Interpersonal Relationships - Celebrate Friends and Family">I</span>
-             <span class="creation-acronym creation-blue has-tip tip-top" data-tooltip aria-haspopup="true" title="OUTLOOK - Practice a Positive, Happy Attitude">O</span>
-             <span class="creation-acronym creation-purple has-tip tip-top" data-tooltip aria-haspopup="true" title="NUTRITION - Fuel Your Body For Health and Wellness">N</span>
-           </div>
+         <div class="columns large-12">
+         <div class="creation-health-acronym-wrapper columns">
+           <h5>What is Creation Health?</h5>
+           <span class="creation-acronym creation-green has-tip tip-top" data-tooltip aria-haspopup="true" title="CHOICE - Make Wise Decisions for Healthy Living">C</span>
+           <span class="creation-acronym creation-orange has-tip tip-top" data-tooltip aria-haspopup="true" title="REST - Enjoy Replenishing Sleep and Relaxation">R</span>
+           <span class="creation-acronym creation-green has-tip tip-top" data-tooltip aria-haspopup="true" title="ENVIRONMENT - Create a Nurturing, Rejuvenating Surrounding">E</span>
+           <span class="creation-acronym creation-orange has-tip tip-top" data-tooltip aria-haspopup="true" title="ACTIVITY - Put Your Body in Motion">A</span>
+           <span class="creation-acronym creation-pink has-tip tip-top" data-tooltip aria-haspopup="true" title="TRUST - Express Your Faith in God">T</span>
+           <span class="creation-acronym creation-red has-tip tip-top" data-tooltip aria-haspopup="true" title="Interpersonal Relationships - Celebrate Friends and Family">I</span>
+           <span class="creation-acronym creation-blue has-tip tip-top" data-tooltip aria-haspopup="true" title="OUTLOOK - Practice a Positive, Happy Attitude">O</span>
+           <span class="creation-acronym creation-purple has-tip tip-top" data-tooltip aria-haspopup="true" title="NUTRITION - Fuel Your Body For Health and Wellness">N</span>
          </div>
-       </div>
-       <div class="columns large-3">
-         <h5>Contact Us</h5>
-         <a href="#">Questions</a> <br> <a href="#">Technical Support</a>
        </div>
      </div>
    </div>
    <div class="footer-sub-menu-wrapper">
-     <img src="http://celebrationhealtheducation.com/wp-content/uploads/2015/09/fh-nav-logo.png">
-     <img src="http://celebrationhealtheducation.com/wp-content/uploads/2015/10/creation-logo-web-e1445309749417.png">
-     <div class="tagline-copyright-wrapper">
-       <p class="tagline"><?php echo get_bloginfo('description'); ?></p>
-       <p class="copyright">&copy; <?php echo date('Y');?>. Celebration Health. All Rights Reserved.</p>
+     <div class="row">
+       <div class="columns large-8 footer-sub-left">
+         <ul class="footer-sub-left-list">
+           <li><a href="#">Forms</a></li>
+           <li><a href="#">Maps</a></li>
+           <li><a href="#">Questions</a></li>
+           <li><a href="#">Technical Support</a></li>
+         </ul>
+       </div>
+       <div class="columns large-4 footer-sub-right">
+         <img src="http://celebrationhealtheducation.com/wp-content/uploads/2015/09/fh-nav-logo.png">
+         <img src="http://celebrationhealtheducation.com/wp-content/uploads/2015/10/creation-logo-web-e1445309749417.png">
+         <div class="tagline-copyright-wrapper">
+           <p class="tagline"><?php echo get_bloginfo('description'); ?></p>
+           <p class="copyright">&copy; <?php echo date('Y');?>. Celebration Health. All Rights Reserved.</p>
+         </div>
+       </div>
      </div>
+
    </div>
  </footer>
 
