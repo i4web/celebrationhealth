b879822881e701730fd8e526610e2c2b164cd958
jrivera <jrivera@i-4web.com>
2015-11-04 23:44:19 -0500

    Remove Manage Patients code
    Left mostly just the foundation HTML and implemented a shortcode.
    
    This will make future theme changes easier and stays consistent with
    other parts of the plugin/theme for Celebration health

--- template-parts/content-manage-patients.php
+++ template-parts/content-manage-patients.php
@@ -7,35 +7,10 @@
 
 ?>
 
-<?php $patients =  I4Web_LMS()->i4_wpcw->i4_get_patients(); //Retrieve the patients from the database ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <div class="page-content-wrapper">
-    <div class="page-title"><h3><?php echo get_the_title();?> <span><a href="#" class="button tiny blue">Add New Patient</a></h3></div>
-
-    <table>
-      <thead>
-        <tr>
-          <th>Patient Username</th>
-          <th>Patient Email</th>
-          <th>Patient Courses</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php foreach($patients as $patient){ //loop through each of the patients
-           $patient_courses = I4Web_LMS()->i4_wpcw->i4_get_assigned_courses($patient->ID); //Retrieve the assigned courses for the patient
-          ?>
-          <tr>
-            <td><?php echo $patient->user_login;?></td>
-            <td><?php echo $patient->user_email;?></td>
-            <td><?php var_dump($patient_courses);?></td>
-            <td><a href="#">Delete Patient</a> <br> <a href="#">Remove Courses</a></td>
-          </tr>
-
-        <?php } ?>
-      </tbody>
-    </table>
+    <?php the_content(); ?>
   </div>
 
 </article>
