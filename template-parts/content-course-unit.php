95f4a92e06188917881c7287da712c964669e86a
jrivera <jrivera@i-4web.com>
2015-11-24 01:47:46 -0500

    Alter unit template to display coordinators
    Changes were made in the plugin to utilize user meta instead of a
    custom table. In order to complete this we also needed to change how we
    displayed the coordinator on the home page.
    
    Using get_user_meta we are able to query the coordinator meta data as
    needed.

--- template-parts/content-course-unit.php
+++ template-parts/content-course-unit.php
@@ -24,8 +24,9 @@ $unit_status = I4Web_LMS()->i4_wpcw->i4_is_unit_complete( $unit_parent_data->cou
 
 //Retrieve the Course Coordinator information
 $i4_course_coordinator = I4Web_LMS()->i4_coordinators->i4_lms_get_coordinator( $unit_parent_data->course_id );
-$i4_coordinator_name = $i4_course_coordinator->coordinator_name;
-$i4_coordinator_email = $i4_course_coordinator->coordinator_email;
+$i4_coordinator_name = $i4_course_coordinator->display_name;
+$i4_coordinator_email = $i4_course_coordinator->user_email;
+$i4_coordinator_img  = get_user_meta($i4_course_coordinator->ID, 'coordinator_img', true);
 
 ?>
 
@@ -49,13 +50,13 @@ $i4_coordinator_email = $i4_course_coordinator->coordinator_email;
         <div class="course-coordinator-wrapper large-12 columns">
           <div class="course-coordinator-details">
               <div class="course-coordinator-img">
-                <img src="<?php echo $i4_course_coordinator->coordinator_img; ?>" alt="Coordinator Name" class="img-circle">
+                <img src="<?php echo $i4_coordinator_img; ?>" alt="Coordinator Name" class="img-circle">
               </div>
                 <h5><?php echo $i4_coordinator_name; ?> <br/><small>Course Coordinator</small></h5>
                 <a href="#" class="button grey tiny" data-reveal-id="askCoordinatorModal">Ask a Question</a>
 
                 <div id="askCoordinatorModal" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
-                    <p class="text-center"><img src="<?php echo $i4_course_coordinator->coordinator_img; ?>" alt="<?php echo $i4_coordinator_name;?>" class="img-circle"></p>
+                    <p class="text-center"><img src="<?php echo $i4_coordinator_img; ?>" alt="<?php echo $i4_coordinator_name;?>" class="img-circle"></p>
                     <h5 class="text-center"><?php echo $i4_coordinator_name; ?><br/><small>Course Coordinator</small></h5>
                   <p id="modalTitle" class="text-center">I'm here to assist you with any questions or concerns that you may have. Just fill out the form below to email me directly.</p>
                   <hr>
